d872cf0fcf774c0c817f1478c23182437e034e89
abnercasallo <68797376+abnercasallo@users.noreply.github.com>
2021-02-09 00:55:12 -0500

    clase1

---
+++ Clase 1/clase1.R
@@ -0,0 +1,15 @@
+#install.packages()
+library(shiny)
+library(shinydashboard)
+
+
+ui<-dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+server <- function(input, output) { 
+}
+
+shinyApp(ui, server)
